326a2507f7e8c3be665107e196911c90862a4fa5
Zach Keeshin <zkeesh@gmail.com>
2013-12-05 17:36:38 -0500

    some testing

--- calc.cpp
+++ calc.cpp
@@ -4,8 +4,6 @@
 #include <cctype>
 using namespace std;
 
-
-
 void handle_math(string op, Dlist<double> &stack){
     if(!stack.isEmpty()){
         double y = stack.removeFront();
@@ -101,7 +99,6 @@ void print_all(Dlist<double> &stack){
 }
 
 void operate(string op, Dlist<double> &stack){
-
     if(op == "+" || op == "-" || op == "*" || op == "/"){
         handle_math(op, stack);
     }else if(op == "n"){
@@ -119,140 +116,14 @@ void operate(string op, Dlist<double> &stack){
     }else{
         cout << "Bad input\n";
     }
-
-
-
-
-
-    // if(op == "n"){
-    //     negate(stack);
-    // }
-
-
-//     double result;
-
-//     if(op == "a"){
-//         stack.print();
-//     }else if(!stack.isEmpty()){
-
-//         if(op == "d"){
-//            //cout << "duplicating... " << endl;
-//            double x = stack.removeFront();
-//            stack.insertFront(x);
-//            stack.insertFront(x);
-//            stack.insertFront(x);
-//            //cout << "printin' stack..." << endl;
-//            //stack.print();
-//         }
-//         if(op == "p"){
-//             double x = stack.removeFront();
-//             cout << x << endl;
-//             stack.insertFront(x);
-//         }
-//         //stack.print();
-        
-        
-// //        if(op == "c"){
-// //            while(!stack.isEmpty()){
-// //                stack.removeFront();
-// //            }
-// //        }else if(op == "q"){
-// //            cout << "going to return..." << endl;
-// //            return;
-// //        }
-//         double x = stack.removeFront();
-// //        cout << "x: " << x << endl;
-// //        if(op == "n"){
-// //            cout << "negating... " << endl;
-// //            result = -x;
-// //            stack.insertFront(result);
-//        //}
-
-// //        if(op == "p"){
-// //            cout << x << endl;
-// //            stack.insertFront(x);
-// //        }
-//         if(!stack.isEmpty()){
-//             if(op == "c"){
-//                 cout << "clearing all... " << endl;
-//                 while(!stack.isEmpty()){
-//                     cout << "removing front..." << endl;
-//                     stack.removeFront();
-//                     cout << "printing... " << endl;
-//                     stack.print();
-//                 }
-//             }else if(op == "q"){
-//                 cout << "going to return..." << endl;
-//                 return;
-//             }
-//             double y = stack.removeFront();
-//             // cout << "y: " << y << endl;
-
-            
-// //            else if(op == "r"){
-// //                cout << "reversing... " << endl;
-// //                stack.insertFront(x);
-// //                stack.insertFront(y);
-// //                cout << "printing..." << endl;
-// //                stack.print();
-// //            }
-            
-//         }else{
-//             //
-//         }
-//     }else{
-//         //cout << "Not enough operands\n";
-//     }
 }
 
-
-
-
-
-
-
-
-// // reading a text file
-// #include <iostream>
-// #include <fstream>
-// #include <string>
-// using namespace std;
-
-// int main () {
-//   string s;
-//   ifstream myfile ("calc.in");
-//   Dlist<double> stack;
-//   if (myfile.is_open())
-//   {
-//     while ( getline (myfile,s) )
-//     {
-//         //cout << s << endl;
-//         if(isdigit(s[0])){
-//             double number = atof(s.c_str());
-//             stack.insertFront(number);
-//         }else{
-//             operate(s, stack);
-//         }
-
-//     }
-//     myfile.close();
-//   }
-
-//   else cout << "Unable to open file"; 
-
-//   return 0;
-// }
-
-
-
 int main() {
-
-    //string input[] = {"2", "3", "4", "+", "*", "p", "+", "d", "+", "p", "2", "-", "p", "q"};
-    string input[] = {"3", "0", "/", "+", "p", "3", "+", "p"};
+    string input[] = {"90", "34", "12", "33", "55", "66", "+", "*", "-", "+", "-", "p"};
     Dlist<double> stack;
     string s;
     int i = 0;
-    while(i < 8){
+    while(i < 12){
         s = input[i];
         i++;
         if(isdigit(s[0])){
@@ -266,5 +137,4 @@ int main() {
         }
         //stack.print();
     }
-
 }
