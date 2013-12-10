2f5d2aa4f6edbdaa236d538f88a71cd1ab8b4c09
Zach Keeshin <zkeesh@gmail.com>
2013-12-10 03:00:12 -0500

    added comments

--- call.cpp
+++ call.cpp
@@ -12,28 +12,23 @@ struct Caller{
 };
 
 void read_file(Dlist<Caller*> & all_events){
-
+	//get number of lines in file
 	int num_lines;
 	cin >> num_lines;
-
+	//loop through and initialize ptr to struct
 	for(int i = 0; i < num_lines; i++){
 		int timestamp;
 		string name;
 		string status;
 		int duration;
-
 		cin >> timestamp >> name >> status >> duration;
-
 		Caller *caller_ptr = new Caller;
     	caller_ptr->timestamp = timestamp;
     	caller_ptr->name = name;
     	caller_ptr->status = status;
     	caller_ptr->duration = duration;
-    
     	all_events.insertBack(caller_ptr); 
-
 	}
-
 }
 
 int main(){
