74a5b5bdc9394895116d0916a33da96028f97107
Zach Keeshin <zkeesh@gmail.com>
2013-12-10 02:46:31 -0500

    it
    reading from file

--- call.cpp
+++ call.cpp
@@ -4,57 +4,39 @@
 #include <cctype>
 using namespace std;
 
-int main(){
-    string s;
-    int i = 0;
-    while(cin >> s){
-    	int j = 0;
-
-    	if(i == 0){
-    		num_lines = atof(s.c_str());
-    		cout << "num lines: " << s << endl;
-    	}else{
-    		string arr[4];
-    		while(j < 4){
-    			arr[j] = s;
-    			cout << arr[j] << endl;
-    			j++;
-    		}
-    	}
-    	i++;
-
-
-    }
-    return 0;
-
-    enum Job {STUDENT, FACULTY, STAFF};
-
-struct Record {
+struct Caller{
+    int timestamp;
     string name;
-    string uniqname;
-    Job job;
+    string status; 
+    int duration;
 };
 
+int main(){
+	int num_lines;
+	cin >> num_lines;
 
-//    Dlist<Record*> wolverineaccess; //records are big, so store pointer
-//    
-//    Record* p = new Record;
-//    p->name = "Andrew DeOrio";
-//    p->uniqname = "awdeorio";
-//    p->job = FACULTY;
-//    wolverineaccess.insertFront( p );
-//    
-//    // // do something with "wolverineaccess"
-//    
-//    // // don't forget to delete objects on the heap
-//    while ( !wolverineaccess.isEmpty() ) {
-//        Record *r = wolverineaccess.removeFront();
-//        cout << r->uniqname << endl;
-//        delete r;
-//    }
+	Dlist<Caller*> all_events;
+
+	for(int i = 0; i < num_lines; i++){
+		int timestamp;
+		string name;
+		string status;
+		int duration;
+
+		cin >> timestamp >> name >> status >> duration;
+
+		Caller *caller_ptr = new Caller;
+    	caller_ptr->timestamp = timestamp;
+    	caller_ptr->name = name;
+    	caller_ptr->status = status;
+    	caller_ptr->duration = duration;
+    
+    	all_events.insertBack(caller_ptr); 
+
+	}
 
 
+	cout << "hello";
+	return 0;
 
 }
-
-//use substring for 
